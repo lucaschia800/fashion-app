095ee5dc3c56343293b574b0800a092287c286a8
lucaschia800 <lucaschia800@gmail.com>
2025-05-27 21:52:32 -0700

    more info

--- relabel_dataset.py
+++ relabel_dataset.py
@@ -13,11 +13,14 @@ prev_label = relabel_df['labelId'].values
 new_label = relabel_df['LabelId_new'].values
 for annotation in annotations:
     labels = annotation['labelId']
+    new_label
     for i in range(len(labels)):
         label = labels[i]
         if label in prev_label:
             index = map_dict[label]
-            labels[i] = new_label[index]
+            
         else:
             print(f'label {label} not found in relabel mapping')
-            
+
+with open('imat_data/val_annos_relabel.json', 'w') as f:
+    json.dump(annotations, f, indent=4)
